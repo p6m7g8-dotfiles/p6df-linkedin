cbe64a325aa46de87a00acbe38390bc422fdbd9f
Philip M. Gollucci <pgollucci@p6m7g8.com>
2026-04-02 23:47:42 -0400

    refactor(init): remove stub profile::mod and fix env::init blank line (#17)

--- init.zsh
+++ init.zsh
@@ -21,9 +21,9 @@ p6df::modules::linkedin::deps() {
 #>
 ######################################################################
 p6df::modules::linkedin::env::init() {
-
   local _module="$1"
   local _dir="$2"
+
   p6_python_path_if "$P6_DFZ_SRC_P6M7G8_DOTFILES_DIR/p6df-linkedin/lib"
 
   p6_return_void
@@ -44,20 +44,4 @@ p6df::modules::linkedin::mcp() {
   p6df::modules::openai::mcp::server::add "linkedin" "npx" "-y" "linkedin-mcp-server"
 
   p6_return_void
-}
-
-######################################################################
-#<
-#
-# Function: words linkedin $LINKEDIN_API_KEY = p6df::modules::linkedin::profile::mod()
-#
-#  Returns:
-#	words - linkedin $LINKEDIN_API_KEY
-#
-#  Environment:	 LINKEDIN_API_KEY
-#>
-######################################################################
-p6df::modules::linkedin::profile::mod() {
-
-  p6_return_words 'linkedin' "$"
 }
